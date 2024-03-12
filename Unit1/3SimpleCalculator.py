f8eb7e6d10995e6f16bdbf8f1bb081e586d2d4d7
Tucker619 <162621786+Tucker619@users.noreply.github.com>
2024-03-12 13:17:14 -0400

    3SimpleCalculator.py

---
+++ Unit1/3SimpleCalculator.py
@@ -0,0 +1,24 @@
+#Add
+print("Welcome to the simple calculator")
+print("This calculator adds two numbers supplied by the user")
+Num1 = int(input("Type first number here "))
+Num2 = int(input("Type second number here "))
+print("Your answer is",(((Num1) + (Num2))))
+#Sub
+print("Welcome to the simple calculator")
+print("This calculator adds two numbers supplied by the user")
+Num1 = int(input("Type first number here "))
+Num2 = int(input("Type second number here "))
+print("Your answer is",(((Num1) - (Num2))))
+#Mult
+print("Welcome to the simple calculator")
+print("This calculator adds two numbers supplied by the user")
+Num1 = int(input("Type first number here "))
+Num2 = int(input("Type second number here "))
+print("Your answer is",(((Num1) * (Num2))))
+#Div
+print("Welcome to the simple calculator")
+print("This calculator adds two numbers supplied by the user")
+Num1 = int(input("Type first number here "))
+Num2 = int(input("Type second number here "))
+print("Your answer is",(((Num1) / (Num2))))
