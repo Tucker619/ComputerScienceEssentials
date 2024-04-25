c891b49810fb0494bd1235efc354bcc24a775fbf
Tucker619 <162621786+Tucker619@users.noreply.github.com>
2024-04-25 13:03:54 -0400

    Create 3MultiplicationCalculator.py

---
+++ Unit1/3MultiplicationCalculator.py
@@ -0,0 +1,5 @@
+print("Welcome to the simple calculator")
+print("This calculator adds two numbers supplied by the user")
+Num1 = int(input("Type first number here "))
+Num2 = int(input("Type second number here "))
+print("Your answer is",(((Num1) * (Num2))))
